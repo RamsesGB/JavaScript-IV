d062d079494a4aeeab24de391e2bc5b323634d8d
Ramses Bermudez <ramsesbermudez@Ramsess-MacBook-Pro.local>
2019-05-09 17:06:16 -0400

    finished creating classes, begining work on creating new objects

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -24,3 +24,46 @@ class Instructor extends Person {
         console.log(`${student.name} receives a perfect score on ${subjectString}`);
     }
 }
+
+class Student extends Person {
+    constructor(obj3){
+        super(obj3);
+        this.previousBackground = obj3.previousBackground;
+        this.className = obj3.className;
+        this.favSubjects = obj3.favSubjects;
+    }
+    listsSubjects(){
+        console.log(this.favSubjects);
+    }
+    PRAssignment(subjectString){
+        console.log(`${this.name} has submitted a PR for ${subjectString}`);
+    }
+    sprintChallenge(subjectString){
+        console.log(`${this.name} has begun sprint challenge on ${subjectString}`);
+    }
+}
+
+class ProjectManager extends Instructor {
+    constructor(obj4){
+        super(obj4);
+        this.gradClassName = obj4.gradClassName;
+        this.favInstructor = obj4.favInstructor;
+    }
+    standUp(slackChannel){
+        console.log(`${this.name} announces to ${slackChannel}, @channel stand up times!​​​​​`);
+    }
+    debugsCode(studentObj, subjectString){
+        console.log(` ${studentObj} debugs ${this.name}'s code on ${subjectString}`)
+    }
+}
+
+const Joshua = new ProjectManager({
+    name: 'Joshua',
+    location: 'California',
+    age: 'Thirties',
+    favLanguage: 'Unity',
+    specialty: 'Kick-ass PM',
+    catchPhrase: 'brady bunch view',
+    gradClassName: 'WEB17',
+    favInstructor: 'Josh Knell',
+})
